7bfd63f3269fb861954a6fc35917158c8cb027c1
ankur <ankur.gr920@gmail.com>
2023-06-18 22:06:36 +0530

    Login screen

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { Button, View, Stack } from "native-base";
+import { Button, View, Stack, Input } from "native-base";
 import { StyleSheet } from 'react-native';
 import React from "react";
 import { Text,  } from "react-native";
@@ -6,10 +6,12 @@ import styles from './Login.style';
 
 const Login = () => {
     return (
-        <Stack style={styles.container}>
-            <Button variant="outline">
-                Lgoin withGoogle
-            </Button>
+        <Stack style={styles.container} space={5} w="75%" maxW="300px" mx="auto">
+            <Text style={{ color: "#ffffff"}}>
+                Login
+            </Text>
+            <Input  placeholder="Username" size="lg" />
+            <Input placeholder="Password" size="lg" />
             <Button variant="ghost">
                 <Text style={{ color: "#ffffff"}}>
                     Dont have an account? Register here...
